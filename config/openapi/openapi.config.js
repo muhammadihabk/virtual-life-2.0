65b2c6c2095d3534c4b55af4760c1e11b41a9f8a
muhammadihabk <69121442+muhammadihabk@users.noreply.github.com>
2023-12-16 20:04:36 +0200

    - Add openapi doc to friend component - Add new endpoint to user component openapi doc

--- config/openapi/openapi.config.js
+++ config/openapi/openapi.config.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const yaml = require('yaml');
 
+const mainOpenapiDoc = fs.readFileSync('config/openapi/openapi.yaml', 'utf8');
+const mainSwaggerDocument = yaml.parse(mainOpenapiDoc);
 const paths = {};
 const components = {
   schemas: {},
   parameters: {},
+  'x-global-variables': mainSwaggerDocument.components['x-global-variables'],
 };
 
 // Add src/user doc
@@ -14,14 +17,25 @@ const userOpenapiDoc = fs.readFileSync(
 );
 const userSwaggerDocument = yaml.parse(userOpenapiDoc);
 const { paths: userPaths } = userSwaggerDocument;
-const { schemas: userSchemas, parameters: userParameters, ...variables } = userSwaggerDocument.components;
+const { schemas: userSchemas, parameters: userParameters, ...userVariables } = userSwaggerDocument.components;
 Object.assign(paths, userPaths);
 Object.assign(components.schemas, userSchemas);
 Object.assign(components.parameters, userParameters);
-Object.assign(components, variables);
+Object.assign(components, userVariables);
 
-const mainOpenapiDoc = fs.readFileSync('config/openapi/openapi.yaml', 'utf8');
-const mainSwaggerDocument = yaml.parse(mainOpenapiDoc);
+// Add src/friend doc
+const friendOpenapiDoc = fs.readFileSync(
+  'src/components/friend/friend.openapi.yaml',
+  'utf8'
+);
+const friendSwaggerDocument = yaml.parse(friendOpenapiDoc);
+const { paths: friendPaths } = friendSwaggerDocument;
+const { schemas: friendSchemas, parameters: friendParameters, ...friendVariables } = friendSwaggerDocument.components;
+Object.assign(paths, friendPaths);
+Object.assign(components.schemas, friendSchemas);
+Object.assign(components.parameters, friendParameters);
+Object.assign(components, friendVariables);
+
 mainSwaggerDocument.paths = paths;
 mainSwaggerDocument.components = components;
 
