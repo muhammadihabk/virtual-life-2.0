94ad3d8b37a2765ba1b0d24186d0be10b316c7ad
muhammadihabk <69121442+muhammadihabk@users.noreply.github.com>
2024-03-26 09:43:05 +0200

    Handle duplicate entry error

--- src/utilities/errorHandlers/internalErrorHandler.js
+++ src/utilities/errorHandlers/internalErrorHandler.js
@@ -1,9 +1,15 @@
 module.exports.internalErrorHandler = function internalErrorHandler(res, error) {
+  // joi validation error
   if (error.details) {
     errorMessages = error.details.map((element) => element.message);
-    res.status(400).json(errorMessages);
+    return res.status(400).json(errorMessages);
+  }
+
+  console.log(error);
+
+  if (error.code === 'ER_DUP_ENTRY') {
+    res.json({ error: 'Duplicate entry' });
   } else {
-    console.log(error);
     res.sendStatus(500);
   }
 };
